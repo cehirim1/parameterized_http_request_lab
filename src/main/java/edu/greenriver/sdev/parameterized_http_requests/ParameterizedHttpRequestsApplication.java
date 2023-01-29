c6681eb61830d698a5176170a16e31aa70a594c3
cehirim1 <Humbled1st@gmail.com>
2023-01-28 19:13:48 -0800

    Committing files to repo - Parameterized http request

---
+++ src/main/java/edu/greenriver/sdev/parameterized_http_requests/ParameterizedHttpRequestsApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.parameterized_http_requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterizedHttpRequestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParameterizedHttpRequestsApplication.class, args);
+    }
+
+}
